e5d85ba430de366a74a8e414e8409bc22d28d9fb
tyseph <sg748@snu.edu.in>
2020-08-21 16:43:22 +0530

    Updates

---
+++ precache-manifest.3fb191b53d03d35fe38c2ef1635b58ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7813dff4c4ca8f8bafb287bd8ec7b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f344799c43b2f4ec448",
+    "url": "/static/css/main.8143f5b8.chunk.css"
+  },
+  {
+    "revision": "63992ebf18afd89df4ad",
+    "url": "/static/js/2.0d0ee6cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0d0ee6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f344799c43b2f4ec448",
+    "url": "/static/js/main.b72aafb8.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  },
+  {
+    "revision": "7c590971f173f0842ef8885b72b6cf26",
+    "url": "/static/media/c.7c590971.png"
+  },
+  {
+    "revision": "5c280ea8cb9504b5d28d46693bb890a4",
+    "url": "/static/media/css.5c280ea8.png"
+  },
+  {
+    "revision": "3801cbe6abb44ae4709a441386e4ca52",
+    "url": "/static/media/html.3801cbe6.png"
+  },
+  {
+    "revision": "afb47678875386aa6e81b65aa0640563",
+    "url": "/static/media/js.afb47678.png"
+  },
+  {
+    "revision": "dc199ecc9e90e4bc7e9ed9b31e3c2b5f",
+    "url": "/static/media/python.dc199ecc.png"
+  },
+  {
+    "revision": "5e49ae1e4c3fc8250115ea1178bc68d9",
+    "url": "/static/media/react.5e49ae1e.png"
+  }
+]);
